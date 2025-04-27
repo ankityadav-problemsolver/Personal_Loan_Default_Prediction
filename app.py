c07f017836b29c8ceb1c8602a2802a0ae77bf6be
ankityadav-problemsolver <ankitofficial151@gmail.com>
2025-04-27 17:42:28 +0530

    fix all issue

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import joblib
 import pandas as pd
 import numpy as np
-import shap
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.metrics import roc_curve, auc
